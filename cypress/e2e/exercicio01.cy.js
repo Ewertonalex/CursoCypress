b19d0817bc66b12ce1d89756b44534fd0f1a4e9f
Ewertonalex <ewertonlgk@gmail.com>
2022-12-03 15:20:56 -0300

    primeirocommit

---
+++ cypress/e2e/exercicio01.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Exercício', () => {
+    it('Validar acesso', () => {        
+        
+        cy.visit('https://demo.automationtesting.in/Register.html')
+
+    })
+
+})
